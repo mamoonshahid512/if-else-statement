701a55944ba6b0c1ca7eb31da4210dd2af983daa
Mamoon Shahid <hafizmamoon2@gmail.com>
2025-02-19 01:30:14 +0500

    Add files via upload

---
+++ if else greater number.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	if(x%5==0 && x%11==0){cout<<by both}
+else if (x%5==0){cout<<by 5 only}
+else if (x%11==0){cout<<by 11 only}
+else {cout<< not by 5 and 11}
+}
